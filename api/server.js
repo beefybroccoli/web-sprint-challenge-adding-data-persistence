00ee488b0b05f00ce05f81dd72545197831f9627
beefybroccoli <tedkimemail@yahoo.com>
2021-12-09 15:28:22 -0800

    ...

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
 // build your server here and require it from index.js
+const express = require("express");
+const server = express();
+server.use(express.json());
+server.use(express.Router());
+
+server.get("/", (req, res)=>{
+    res.status(200).json({message:"hello from server"});
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // start your server here
+const server = require("./api/server");
+const PORT = 9000;
+
+server.listen(PORT, ()=>{
+    console.log(`server listening on PORT ${PORT}`);
+})
