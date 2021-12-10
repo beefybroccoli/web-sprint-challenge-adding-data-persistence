09dfedaa353643cc38ad81cd5fc4ffaba86d050f
beefybroccoli <tedkimemail@yahoo.com>
2021-12-10 14:16:14 -0800

    met mvp

--- api/server.js
+++ api/server.js
@@ -6,7 +6,6 @@ const routerResources = require("./resource/router");
 const routerTasks = require("./task/router");
 
 server.use(express.json());
-server.use(express.Router());
 
 server.get("/", (req, res)=>{
     res.status(200).json({message:"hello from server"});
@@ -16,7 +15,7 @@ server.use("/api/projects", routerProjects);
 server.use("/api/resources", routerResources);
 server.use("/api/tasks", routerTasks);
 
-server.use((req, res) =>{
+server.get((req, res) =>{
     res.status(404).json({message:`invalid path ${req.path}`});
 })
 
