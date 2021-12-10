575fcb93e5a8a646c30c292a7e23bd2204101ed8
beefybroccoli <tedkimemail@yahoo.com>
2021-12-09 20:31:19 -0800

    ...

--- api/project/router.js
+++ api/project/router.js
@@ -1,4 +1,3 @@
-// build your `/api/projects` router here
 const express = require('express');
 const router = express();
 const modelProjects = require("./model");
@@ -7,9 +6,6 @@ const {errorHandler}=require("../errorHandler");
 router.use(express.json());
 router.use(express.Router());
 
-// - [ ] `[GET] /api/projects`
-//   - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
-//   - Example of response body: `[{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}]`
 router.get("", async (req, res, next)=>{
     try{
         const array = await modelProjects.getAll();
@@ -20,9 +16,6 @@ router.get("", async (req, res, next)=>{
     }
 })
 
-// - [ ] `[POST] /api/projects`
-//   - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
-//   - Example of response body: `{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}`
 router.post("", verifyNewProject, async (req, res, next)=>{
     try{
         const newProjectId = await modelProjects.insert(req.newProject);
